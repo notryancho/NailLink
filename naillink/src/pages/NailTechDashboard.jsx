49edef1cd0f06af63c9a6138b90e8d93f751b3f6
notryancho <ryancho1226@hotmail.com>
2023-03-31 15:43:15 -0400

    nailtech dashboard page

---
+++ naillink/src/pages/NailTechDashboard.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const NailTechDashboard = () => {
+  const [nailTech, setNailTech] = useState(null);
+
+  useEffect(() => {
+    axios.get('/api/nailtech').then((response) => {
+      setNailTech(response.data);
+    });
+  }, []);
+
+  if (!nailTech) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Welcome, {nailTech.name}!</h1>
+      <h2>Your Appointments</h2>
+      <ul>
+        {nailTech.appointments.map((appointment) => (
+          <li key={appointment.id}>
+            {appointment.appt_date} at {appointment.appt_time} with{' '}
+            {appointment.customer_name}
+          </li>
+        ))}
+      </ul>
+      <h2>Your Reviews</h2>
+      <ul>
+        {nailTech.reviews.map((review) => (
+          <li key={review.id}>
+            <p>Rating: {review.rating}</p>
+            <p>Comment: {review.comment}</p>
+            <p>By: {review.customer_name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default NailTechDashboard;
