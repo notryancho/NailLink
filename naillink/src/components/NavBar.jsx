9ac5808b39d2048df883e35e9e400a8797b9b28b
notryancho <ryancho1226@hotmail.com>
2023-03-31 16:23:34 -0400

    navbar

---
+++ naillink/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/signup">Sign Up</Link>
+        </li>
+        <li>
+          <Link to="/login">Log In</Link>
+        </li>
+        {/* Add more links here */}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
