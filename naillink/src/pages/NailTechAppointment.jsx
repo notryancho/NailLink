e3d77161e462664b1f2e4c60742dfd9fd6e10c5d
notryancho <ryancho1226@hotmail.com>
2023-03-31 15:45:21 -0400

    nailtech appointment page

---
+++ naillink/src/pages/NailTechAppointment.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const NailTechAppointments = () => {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    const getAppointments = async () => {
+      const response = await axios.get("/api/appointments");
+      setAppointments(response.data);
+    };
+
+    getAppointments();
+  }, []);
+
+  return (
+    <div>
+      <h1>My Appointments</h1>
+      <ul>
+        {appointments.map((appointment) => (
+          <li key={appointment.id}>
+            <p>Date: {appointment.appt_date}</p>
+            <p>Time: {appointment.appt_time}</p>
+            <p>Customer: {appointment.customer_id.name}</p>
+            <p>Status: {appointment.status}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default NailTechAppointments;
