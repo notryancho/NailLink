e71c0b5511433fd03262cba58a7fd1ae7fc8dc79
notryancho <ryancho1226@hotmail.com>
2023-04-07 05:26:55 +0900

    edit navbar

--- naillink/src/components/NavBar.jsx
+++ naillink/src/components/NavBar.jsx
@@ -8,15 +8,15 @@ const NavBar = ({ user, handleLogout }) => {
     <nav className="navbar">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/" className="nav-link">HOME</Link>
         </li>
         {!localStorage.getItem('id') ? 
         <>
         <li className="nav-item">
-          <Link to="/signup" className="nav-link">Sign Up</Link>
+          <Link to="/signup" className="nav-link">SIGN UP</Link>
         </li>
         <li className="nav-item">
-          <Link to="/login" className="nav-link">Log In</Link>
+          <Link to="/login" className="nav-link">LOG IN</Link>
         </li></> : 
         <li className="nav-item" onClick={handleLogout}><a href="/" className="nav-link">Log Out</a></li>}
         {localStorage.getItem("user") === "customer" && <>
